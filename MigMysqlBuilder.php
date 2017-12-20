50f39384a778399fa15912dda0bc53941f0023e4
lenochware <lenochware@gmail.com>
2017-12-20 12:27:32 +0100

    MigMysqlBuilder implementation

--- MigMysqlBuilder.php
+++ MigMysqlBuilder.php
@@ -4,29 +4,36 @@ require 'MigSqlBuilder.php';
 
 class MigMysqlBuilder extends MigSqlBuilder
 {
+  function dropTable($name)
+  {
+  	$this->sql[] = "DROP TABLE `$name`";
+  }
+
+	function dropColumn($table, $name)
+	{
+		$this->sql[] = "ALTER TABLE `$table` DROP COLUMN `$name`";
+	}
+
+	function addColumn($table, $name, $def)
+	{
+		$sql[] = "ALTER TABLE `$table` ADD `$name`";
+		$sql[] = $def['type'];
+		if ($def['extra']) $sql[] = $def['extra'];
+		$sql[] = "default ". ($def['default'] ?: 'NULL');
+
+		$this->sql[] = implode(' ', $sql);
+	}
+
+	function changeColumn($table, $name, $def) {
+
+	}
 
  	function createTable($name, $def) {
 
  	}
 
-  function dropTable($name) {
-  	$this->sql[] = "DROP TABLE `$name`";
-
-  }
-
-	function updateTable($name, $data) {
-
-	}
-
-	function addColumn($table, $name, $def) {
-
-	}
-
-	function dropColumn($table, $name) {
-
-	}
-
-	function changeColumn($table, $name, $def) {
+	function updateTable($name, $data)
+	{
 
 	}
 
